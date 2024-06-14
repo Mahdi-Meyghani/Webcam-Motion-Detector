863ab0224ac661e675581467ba1f856348418fec
Mahdi <mahdimeyghani02@gmail.com>
2024-06-14 14:48:50 +0330

    Create gray and blur frame

--- main.py
+++ main.py
@@ -10,4 +10,14 @@ count = 1
 
 while True:
     check, frame = cap.read()
+
+    status = 0
+
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_blur = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+
+    if first_frame is None:
+        first_frame = gray_frame_blur
+
+
     cv2.imshow("Object Detection", frame)
