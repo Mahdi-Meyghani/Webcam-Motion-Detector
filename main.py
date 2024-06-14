5d00e7ba9c37a20a1f6d7169c706535aac49e139
Mahdi <mahdimeyghani02@gmail.com>
2024-06-14 14:49:35 +0330

    Find differences between frames

--- main.py
+++ main.py
@@ -19,5 +19,8 @@ while True:
     if first_frame is None:
         first_frame = gray_frame_blur
 
+    differences_frame = cv2.absdiff(first_frame, gray_frame_blur)
+    thresh_frame = cv2.threshold(differences_frame, 60, 255, cv2.THRESH_BINARY)[1]
+    dilate_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
     cv2.imshow("Object Detection", frame)
