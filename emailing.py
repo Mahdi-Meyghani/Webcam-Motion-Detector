29e4cd8ad29d5cda8700ec146fc5669341ea7fc9
Mahdi <mahdimeyghani02@gmail.com>
2024-06-14 15:01:07 +0330

    Test send_email function.

--- emailing.py
+++ emailing.py
@@ -26,3 +26,7 @@ def send_email(img_path):
     email.login(USER, PASSWORD)
     email.sendmail(USER, RECEIVER, email_msg.as_string())
     email.quit()
+
+
+if __name__ == "__main__":
+    send_email("images/new.jpg")
